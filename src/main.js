4a9a1d5293ed88a2d8d5851ddb817ae7cced2e38
Alexander Hill <alexhill@motivateco.com>
2018-03-22 11:36:34 -0400

    HTML and JS example.

---
+++ src/main.js
@@ -0,0 +1,14 @@
+//setTimeout(function() { alert('Buy now!') })
+
+//var button = document.getElementsByTagName('button')[0]
+
+//button.onclick = function() {
+  //var list = document.getElementById('numbers-list')
+  //var count = list.children.length
+
+  //var newListItem = document.createElement('li')
+  //var newNumber = document.createTextNode(count + 1)
+
+  //newListItem.appendChild(newNumber)
+  //list.appendChild(newListItem)
+//}
