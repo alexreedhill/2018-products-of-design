e41d62d85cb87b6726a27e5cf23e163194116938
Alexander Hill <alexhill@motivateco.com>
2019-03-19 17:21:44 -0400

    Improve contrived example, add animate.css.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
-//setInterval(function() { alert('Buy now!') }, 1000)
-
 var button = document.getElementsByTagName('button')[0]
 
 button.onclick = function() {
-  var list = document.getElementById('numbers-list')
+  var list = document.getElementsByClassName('numbers-list')[0]
   var count = list.children.length
 
   var newListItem = document.createElement('li')
   var newNumber = document.createTextNode(count + 1)
+
+  //newListItem.classList.add('animated', 'fadeIn')
 
   newListItem.appendChild(newNumber)
   list.appendChild(newListItem)
